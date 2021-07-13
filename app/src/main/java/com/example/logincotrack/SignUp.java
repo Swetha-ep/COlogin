c94889e854a36dea31807cce478f60a1fd0a23ae
Shruthi P <shruthisp99@gmail.com>
2021-07-13 11:19:55 +0530

    COTRACK LOGIN

---
+++ app/src/main/java/com/example/logincotrack/SignUp.java
@@ -0,0 +1,42 @@
+package com.example.logincotrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SignUp extends AppCompatActivity {
+
+
+    TextInputLayout regName, regUsername, regEmail, regPhoneNo, regPassword;
+    Button regBtn, regToLoginBtn;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+    }
+
+
+    public void registerUser(View view) {
+
+        String name = regName.getEditText().getText().toString();
+        String username = regUsername.getEditText().getText().toString();
+        String email = regEmail.getEditText().getText().toString();
+        String phoneNo = regPhoneNo.getEditText().getText().toString();
+        String password = regPassword.getEditText().getText().toString();
+        UserHelperClass helperClass = new UserHelperClass(name,username,email,phoneNo,password);
+        reference.child(username).setValue(helperClass);
+    }
+
+
+}
